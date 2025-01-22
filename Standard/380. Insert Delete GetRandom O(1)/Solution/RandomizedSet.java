69a68ffc5533ba5b15443f848bb939552f73a7bf
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2025-01-22 15:36:31 +0800

    add more solutions

--- Standard/380. Insert Delete GetRandom O(1)/Solution/RandomizedSet.java
+++ Standard/380. Insert Delete GetRandom O(1)/Solution/RandomizedSet.java
@@ -1,19 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+
 public class RandomizedSet {
 
+    HashMap<Integer, Integer> hashMap;
+    ArrayList<Integer> arrayList;
+    Random random = new Random();
+
     public RandomizedSet() {
-        
+        hashMap = new HashMap<>();
+        arrayList = new ArrayList<>();
     }
     
     public boolean insert(int val) {
-        
+        if (hashMap.containsKey(val)) {
+            return false;
+        } else {
+            hashMap.put(val, arrayList.size());
+            arrayList.add(val);
+            return true;
+        }
     }
     
     public boolean remove(int val) {
-        
+        if (hashMap.containsKey(val)) {
+            int index = hashMap.get(val);
+            int last = arrayList.getLast();
+            arrayList.set(index, last);
+            hashMap.put(last, index);
+            hashMap.remove(val);
+            arrayList.removeLast();
+            return true;
+        } else {
+            return false;
+        }
     }
     
     public int getRandom() {
-        
+        return arrayList.get(random.nextInt(arrayList.size()));
     }
 }
 
@@ -24,6 +50,3 @@ public class RandomizedSet {
  * boolean param_2 = obj.remove(val);
  * int param_3 = obj.getRandom();
  */
-{
-
-}
