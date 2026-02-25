ce486f191e079dd9594b5dc7702c1299b6599c6c
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2026-02-25 23:21:54 +0800

    933. Number of Recent Calls

---
+++ Standard/933. Number of Recent Calls/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class RecentCounter {
+    private final Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new ArrayDeque<>();
+    }
+
+    public int ping(int t) {
+        while (!queue.isEmpty() && t - queue.peek() > 3000) {
+            queue.poll();
+        }
+
+        queue.offer(t);
+
+        return queue.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
